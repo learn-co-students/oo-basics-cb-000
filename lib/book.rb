2834da07edb9507e0c41b964b3ad37b5f6d9c568
Kyle Much <kylemuch@gmail.com>
2017-01-28 21:50:44 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
 
+  attr_accessor :author,:page_count,:genre
 
+  def initialize(title)#initialize triggers automatically when any object with the Book class is created. (whenver new is called on a class)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
