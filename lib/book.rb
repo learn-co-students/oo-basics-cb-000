ac8d4f54c88138dc5c9c98bdc87a0c897ecd419d
Martyn Conkling <haoleboy3000@yahoo.com>
2017-08-24 10:30:59 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,46 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  #attr_accessor :author
+  #attr_accessor :page_count
+  #attr_accessor :genre
 
 
+
+
+
+
+end
