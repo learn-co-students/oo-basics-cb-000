857bdbf18c8aa8bd6e1552cbdec608c174cba86f
Jen Poole <contact.vandepool@gmail.com>
2017-11-04 23:08:56 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,11 @@
 # Book::new gets initialized with a title
 class Book
+  # has properties
+  attr_accessor :title, :author, :genre, :page_count
 
   # accept an argument on initialization
   def initialize(title)
+    @title = title
   end
 end
 
