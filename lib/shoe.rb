b4f8bf46b1d7e8954c0cc1261474da748209dc35
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-19 14:05:03 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,8 @@
 
 class Shoe
  
- attr_accessor :brand, :color, :size, :material, :condition,
+ attr_accessor :brand, :color, :size, :material, :condition
+
 
 def initialize (brand)
   @brand= brand
